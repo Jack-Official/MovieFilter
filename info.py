7bdcd01ede83c83fe939e5771bdebdaf3a5aa0c8
MANTIS ™️ <159612843+MANTIS-001@users.noreply.github.com>
2024-02-12 11:04:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,7 +26,7 @@ BOT_TOKEN = environ['BOT_TOKEN']
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 9999))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
-PICS = (environ.get('PICS' ,'https://telegra.ph/file/79bd83439254ed9c4bbca.jpg')).split()
+PICS = (environ.get('PICS' ,'https://telegra.ph/file/56a0ea670f85e26e69226.jpg')).split()
 NOR_IMG = environ.get('NOR_IMG', "https://graph.org/file/ebecf2e8bd05f866ea862.jpg")
 BOT_START_TIME = time()
 
